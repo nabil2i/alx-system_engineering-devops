10ddb3139299cde07bd6164e2e1ed65828984a88
Nabil <nabil.noble01@gmail.com>
2022-09-27 11:20:20 +0000

    Edit

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -6,7 +6,7 @@ package { 'nginx':
 file_line { 'aaaaa':
   ensure => 'present',
   path   => '/etc/nginx/sites-available/default',
-  after  => 'listen 80 default_server;',
+  after  => 'server_name _;',
   line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
 }
 
