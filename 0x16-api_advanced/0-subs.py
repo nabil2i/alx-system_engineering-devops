a64fa53d259269d3fa0121e886b3825cd5306826
Nabil <nabil.noble01@gmail.com>
2022-11-01 22:02:09 +0000

    Edit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,12 +9,13 @@ import requests
 def number_of_subscribers(subreddit):
     """function queries the Reddit API and 
     returns the number of subs for a given subreddit"""
-    if subreddit is None of type(subreddit) is not str:
+    if subreddit is None or type(subreddit) is not str:
         return 0
-    r = requests.get("https://www.reddit.com/r/{}/about.json".format(subreddit),
-                     allow_redirects=False).json()
-    r.headers["User-Agent": "custom user agent"]
+    r = requests.get("https://www.reddit.com/r/{}/about.json"
+                            .format(subreddit),
+                            headers={"User-Agent": "My-User-Agent"},
+                            allow_redirects=False)
     if r.status_code >= 300:
         return 0
-    
-    return r..get("data").get("subscribers")
+
+    return r.json().get("data").get("subscribers")
