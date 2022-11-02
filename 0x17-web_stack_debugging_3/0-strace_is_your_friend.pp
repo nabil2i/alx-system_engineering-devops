d220bae4922783647068c74715f1d36adc2129f7
Nabil <nabil.noble01@gmail.com>
2022-11-02 14:50:23 +0000

    Edit

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
-# Fix typo error in wordpress site running on apache2
-exec { 'fix wordpress typo error':
-	command => "sudo sed -i 's/phpp/php/g' /var/www/html/wp-settings.php; sudo service apache2 restart",
-	path => ['/usr/local/bin', '/usr/local/sbin', '/sbin' , '/bin', '/usr/bin', '/usr/sbin']
+# Fixes typo error in wordpress site
+exec { 'fix-wordpress':
+  command => "sudo sed -i 's/phpp/php/g' /var/www/html/wp-settings.php; sudo service apache2 restart",
+  path    => ['/bin', '/usr/bin', '/usr/sbin']
 }
