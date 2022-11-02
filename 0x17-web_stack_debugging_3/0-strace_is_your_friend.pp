c77ef3e22568cda23aba6f383b4035b931565af1
Nabil <nabil.noble01@gmail.com>
2022-11-02 14:50:54 +0000

    Edit

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 # Fixes typo error in wordpress site
-exec { 'fix-wordpress':
+exec { 'fix-typo-error':
   command => "sudo sed -i 's/phpp/php/g' /var/www/html/wp-settings.php; sudo service apache2 restart",
   path    => ['/bin', '/usr/bin', '/usr/sbin']
 }
