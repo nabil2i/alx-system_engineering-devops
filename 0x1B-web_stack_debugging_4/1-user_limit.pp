d439df3ba2d1cf6a2d1e9c5fc615b2ab66f4a637
Nabil <nabil.noble01@gmail.com>
2022-11-07 19:27:27 +0000

    Edit

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,5 @@
+# Puppet manifest to fix Too many open files
+exec { 'fix-toomanyfiles':
+    command => "sed -i 's/holberton hard nofile 5/holberton hard nofile 1605972/g' /etc/security/limits.conf; sed -i 's/holberton soft nofile 4/holberton soft nofile 1048576/g' /etc/security/limits.conf",
+    path    => ['/bin', '/usr/bin', '/usr/sbin']
+}
