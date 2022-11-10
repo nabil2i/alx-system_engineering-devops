d56c63ff9e790eb3305dcdb9db53051de3cd7976
Nabil <28634540+nabil2i@users.noreply.github.com>
2022-11-10 13:07:45 +0000

    Update 1-user_limit.pp

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,5 +1,6 @@
 # Puppet manifest to fix Too many open files
 exec { 'fix-toomanyfiles':
-    command => "sed -i 's/holberton hard nofile 5/holberton hard nofile 1048576/g' /etc/security/limits.conf; sed -i 's/holberton soft nofile 4/holberton soft nofile 1048576/g' /etc/security/limits.conf",
+    command => "sed -i 's/holberton hard nofile 5/holberton hard nofile 1605972/g' /etc/security/limits.conf; sed -i 's/holberton soft nofile 4/holberton soft nofile 1605972/g' /etc/security/limits.conf;
+sed -i -e '$asession required pam_limits.so' /etc/pam.d/common-session; sed -i -e '$asession required pam_limits.so' /etc/pam.d/common-session-noninteractive ",
     path    => ['/bin', '/usr/bin', '/usr/sbin']
 }
